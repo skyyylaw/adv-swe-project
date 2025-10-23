d584fa8e62851857284eacdb9159f8d3a7524d35
Michael Mihaley <mjm2442@columbia.edu>
2025-10-23 17:22:40 -0400

    removed old folders

--- src/main/java/dto/goal/GoalTreeNode.java
+++
@@ -1,4 +0,0 @@
-package dto.goal;
-
-public class GoalTreeNode {
-}
--- src/main/java/models/GoalStatus.java
+++
@@ -1,6 +0,0 @@
-package models;
-
-// This class defines the possible values for the status of a goal
-public enum GoalStatus {
-  ACTIVE, COMPLETED, ARCHIVED
-}
